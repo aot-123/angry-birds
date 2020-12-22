6e33dac6d8cce9e9e4e4a21a194a5b513635e9e6
aot-123 <75157901+aot-123@users.noreply.github.com>
2020-12-22 14:59:42 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,18 @@
+class Bird extends BaseClass {
+    constructor(x, y) {
+      var options = {
+        'density':1.5,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      super(x,y,50,50);
+      this.image = loadImage("sprites/bird.png");
+      
+    };
+    display(){
+      this.body.position.x = mouseX;
+      this.body.position.y = mouseY;
+      super.display();
+    };
+  };
+  
